93c103f7b3546a41e39ad3392b98df248139c5ac
Muhammad-gf <muhammadjonmaseyev@gmail.com>
2026-01-20 00:58:08 +0500

    add - AuthApi service

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Mango.Services.AuthAPI.Models;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
